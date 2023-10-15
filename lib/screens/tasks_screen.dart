7e3b04993efede2d6d859140050503fc3d034806
programmingio <117992080+programmingio@users.noreply.github.com>
2023-10-15 23:46:43 +0530

    base ui desing of task screen done

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,8 +5,60 @@ class TasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.greenAccent,
-    );
+    return Scaffold(
+        backgroundColor: Colors.greenAccent,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(
+                  top: 50.0, left: 20.0, right: 20.0, bottom: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                      radius: 25.0,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.list,
+                        color: Colors.greenAccent,
+                      )),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    'Todoey',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 45.0,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Text(
+                    '12 Tasks',
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(20.0))),
+              ),
+            )
+          ],
+        ));
   }
 }
