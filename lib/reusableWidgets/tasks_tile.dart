3beacaa19c0f975e0bdae14a06ae78bef16fcc5d
programmingio <117992080+programmingio@users.noreply.github.com>
2023-10-20 00:20:55 +0530

    created and designed the modalBottomSheet triggered with floating action button (functionality of adding new task not done right now)

--- lib/reusableWidgets/tasks_tile.dart
+++ lib/reusableWidgets/tasks_tile.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 
-class ReusableTaskTile extends StatelessWidget {
-  const ReusableTaskTile({
-    super.key,
-  });
+class ReusableTaskTile extends StatefulWidget {
+  const ReusableTaskTile({super.key});
+
+  @override
+  State<ReusableTaskTile> createState() => _ReusableTaskTileState();
+}
+
+class _ReusableTaskTileState extends State<ReusableTaskTile> {
+  // now thats look like clean code //
+  bool? valueOfCheckbox = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      trailing:
-      Checkbox(value: false, onChanged: (bool? value) {}),
-      title: Text('this is a demo task'),
+      trailing: Checkbox(
+          value: valueOfCheckbox,
+          activeColor: Colors.greenAccent,
+          onChanged: (newValue) {
+            setState(() {
+              valueOfCheckbox = newValue;
+            });
+          }),
+      title: Text(
+        'this is a demo task',
+        style: TextStyle(
+            decoration: valueOfCheckbox == false
+                ? TextDecoration.none
+                : TextDecoration.lineThrough),
+      ),
     );
   }
 }
